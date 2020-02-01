546f40f481f0a478f0046a967ce13e0ee5fe2514
James Merrix <james@appoly.co.uk>
2020-02-01 22:43:10 +0000

    Adding admin route prefix.

--- src/routes.php
+++ src/routes.php
@@ -3,5 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::post('/admin/pages/image-upload', 'Appoly\LaravelPages\Http\Controllers\PagesController@imageUpload');
+Route::name(config('laravel_pages.admin_route_prefix'))->group(function () {
     Route::resource(config('laravel_pages.admin_pages_route'), 'Appoly\LaravelPages\Http\Controllers\PagesController')->except('show');
-    Route::get(config('laravel_pages.view_pages_route'), 'Appoly\LaravelPages\Http\Controllers\PagesController@show')->name('laravel-pages.show');
+});
+Route::get(config('laravel_pages.view_pages_route'), 'Appoly\LaravelPages\Http\Controllers\PagesController@show')->name('laravel-pages.show');
